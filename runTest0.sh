47516c22395a1fce047a513f8a03cd88212bccdd
clauk <claus.trejo@gmail.com>
2018-04-20 13:50:37 +0200

    Modify bash file

--- runTest0.sh
+++ runTest0.sh
@@ -7,8 +7,8 @@ virtualenv $VIRTUAL_ENV_NAME
 echo '#### Activate Virtual Environment ####'
 source $VIRTUAL_ENV_NAME/bin/activate
 
-echo '#### Install requirements ####'
-pip install -r requirements.txt 
+#echo '#### Install requirements ####'
+#pip install -r requirements.txt 
 
 echo '#### Run Test ####'
 py.test --headless test/login_scenario.py
